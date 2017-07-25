ed0daaf4ed3249bdf83e2e08aac501888d8fde28
David Campelo <david.campelo@gmail.com>
2017-07-25 12:29:41 +0100

    Created Recommender and Evalator for Alura courses.

---
+++ src/main/java/org/davidcampelo/ai/MyAluraCoursesRecommenderEvaluator.java
@@ -0,0 +1,33 @@
+package org.davidcampelo.ai;
+
+import java.io.IOException;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.common.RandomUtils;
+import org.davidcampelo.ai.rs.SimpleDataModel;
+import org.davidcampelo.ai.rs.SimpleRecommenderBuilder;
+
+public class MyAluraCoursesRecommenderEvaluator {
+	public static void main(String[] args) throws IOException {
+		
+		RandomUtils.useTestSeed();
+	
+        RecommenderEvaluator recommenderEvaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+
+        DataModel dataModel = SimpleDataModel.createAluraCoursesModel(); 
+		
+        double error = -1;
+		try {
+			error = recommenderEvaluator.evaluate(new SimpleRecommenderBuilder(), null, dataModel, 0.9, 1.0);
+		} catch (TasteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(error);
+	}
+
+}
