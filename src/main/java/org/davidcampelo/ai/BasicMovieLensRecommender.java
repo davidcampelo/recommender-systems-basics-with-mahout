57edf3222293206839411497647aefdb39c33774
David Campelo <david.campelo@gmail.com>
2017-07-26 22:48:51 +0100

    Using mahout and slf4j libraries now

--- src/main/java/org/davidcampelo/ai/BasicMovieLensRecommender.java
+++ src/main/java/org/davidcampelo/ai/BasicMovieLensRecommender.java
@@ -2,8 +2,7 @@ package org.davidcampelo.ai;
 
 import java.io.IOException;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
+
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
@@ -11,14 +10,14 @@ import org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.davidcampelo.ai.rs.SimpleDataModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BasicMovieLensRecommender 
 {
-	private static Logger logger = Logger.getLogger(BasicMovieLensRecommender.class); 
+	private static Logger logger = LoggerFactory.getLogger(BasicMovieLensRecommender.class); 
     public static void main( String[] args ) throws IOException, TasteException
     {
-    	// log4j
-    	BasicConfigurator.configure();
     	logger.info("**** Creating Item Based Recommender...");
     	// mahout data model
     	DataModel dataModel = SimpleDataModel.createMovieLensModel();
@@ -27,12 +26,12 @@ public class BasicMovieLensRecommender
     	// recommender
     	GenericItemBasedRecommender basedRecommender = new GenericItemBasedRecommender(dataModel, coefficientSimilarity);
     	// do it!
-    	Logger.getRootLogger().info("**** Showing the most similar itens of 10 items...");
+    	logger.info("**** Showing the most similar itens of 10 items...");
     	int count = 0;
     	for (LongPrimitiveIterator it = dataModel.getItemIDs(); it.hasNext() && count++ < 10; ) {
     		long itemID = it.nextLong();
     		for (RecommendedItem item : basedRecommender.mostSimilarItems(itemID, 10) ) {
-    			System.out.println("ITEM ID = " + itemID + "\tMOST SIMILAR ITEM ID = " +item.getItemID() + "\t" + "SIMILARITY VALUE = "+ item.getValue());
+    			logger.info("ITEM ID = " + itemID + "\tMOST SIMILAR ITEM ID = " +item.getItemID() + "\t" + "SIMILARITY VALUE = "+ item.getValue());
 			}
     	}
         
